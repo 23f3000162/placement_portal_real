d62d7e16926c71de3cac193ca3c6c76adaba9ead
Anshu Sharma <195125907+23f3000162@users.noreply.github.com>
2026-07-05 14:09:24 +0000

    Create application tracking model

---
+++ backend/models/applications.py
@@ -0,0 +1,13 @@
+from extension import db 
+from datetime import datetime
+class Application(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    drive_id = db.Column(db.Integer, db.ForeignKey('drive.id'), nullable=False)
+    resume_file = db.Column(db.String(255))
+    status = db.Column(db.String(20),default="pending")
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    interview_scheduled_at = db.Column(db.DateTime, nullable=True)
+    interview_mode = db.Column(db.String(50), nullable=True)
+    interview_location = db.Column(db.String(255), nullable=True)
+    
