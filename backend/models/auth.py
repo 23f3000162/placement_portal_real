b6825a553e263c0a978e9c3b598f50b938175acb
Anshu Sharma <195125907+23f3000162@users.noreply.github.com>
2026-07-05 14:08:55 +0000

    Add user authentication model

---
+++ backend/models/auth.py
@@ -0,0 +1,14 @@
+from extension import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True,nullable=False)
+    password = db.Column(db.String(240),nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+    is_blocked = db.Column(db.Boolean, default=False)
+    cgpa = db.Column(db.Float, nullable=True)
+    branch = db.Column(db.String(100), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
